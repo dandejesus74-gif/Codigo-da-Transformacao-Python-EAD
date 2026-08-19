2fa590894b02564bfc05cb48a2282f4bfa1b2742
aluno vocacao <dandejesus74@gmail.com>
2026-08-19 16:29:23 -0300

    update_no mudulo07
    na aula de hoje fizemos as ativadadesdo modulo07

---
+++ moduloo7/modulo07_ativ02_explore_bibliotecas_Daniel.py
@@ -0,0 +1,17 @@
+import utilidades
+
+num1 = 10
+num2 = 5
+
+print('"""Teste De Utilidades"""')
+print(f'Números Utilizados: {num1} e {num2}')
+
+print(f' Usando Adição({num1} + {num2}):', utilidades.soma(num1, num2))
+print(f' Usando subtracão({num1} - {num2}):', utilidades.subtrair(num1, num2))
+print(f' Usando Multiplicação({num1} * {num2}):', utilidades.multiplicar(num1, num2))
+print(f' Usando divisao({num1} / {num2}):', utilidades.dividir(num1, num2))
+print(f' Usando divisao_inteira({num1} // {num2}):', utilidades.divisao_inteira(num1, num2))
+print(f' Usando resto_divisao({num1} % {num2}):', utilidades.resto_divisao(num1, num2))
+print(f' Usando potencia({num1} ** {num2}):', utilidades.potencia(num1, num2))
+
+print(f' Usando calcular_media({num1} e {num2}):', utilidades.calcular_media(num1, num2))
---
+++ moduloo7/utilidades.py
@@ -0,0 +1,59 @@
+
+'''
+'''
+
+
+
+
+
+def soma(a, b):
+
+    return a + b
+
+def subtrair(a, b):
+
+    return a - b
+
+
+def multiplicar(a,b):
+
+    return a * b
+
+
+def dividir(a, b):
+
+    if b == 0:
+        return "Erro: Divisão Por Zero Não Permitida"
+    return a / b
+
+def divisao_inteira (a, b):
+    if b == 0:
+        return "Erro: Divisão Por Zero Não é Permitida"
+    return a // b
+
+def resto_divisao(a, b):
+    if b == 0:
+        return "Erro: Divisão Por Zero Não é Permitida."
+    return a % b
+
+def potencia(base, expoente):
+    return base ** expoente
+
+def calcular_media(lista_numeros):
+    if not lista_numeros:
+        return 0
+    return sum(lista_numeros) / len(lista_numeros)
+
+
+
+def calcular_media(lista_numeros):
+
+    if not lista_numeros:
+        return 0
+    return sum(lista_numeros) /len(lista_numeros)
+
+
+def e_par(numero):
+    return numero % 2 == 0
+
+
